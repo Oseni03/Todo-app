9b8a520958ccaf0b4291a5843003cf12e86c465e
Oseni03 <oseniayomide57@gmail.com>
2022-09-02 18:00:15 +0100

    Settings updated

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("TODO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get("TODO_DEBUG_VALUE") == "True")
+DEBUG = (os.environ.get("DEBUG_VALUE") == "True")
 
 ALLOWED_HOSTS = ["localhost", "tasks-organizar.herokuapp.com"]
 
